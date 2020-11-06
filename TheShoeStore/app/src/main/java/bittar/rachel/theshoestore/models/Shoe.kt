485eeb255baf670c85387c283ebaf1200ab1374e
Rachel Bittar <rachelbbittar@gmail.com>
2020-11-06 14:22:40 -0500

    first commit

---
+++ TheShoeStore/app/src/main/java/bittar/rachel/theshoestore/models/Shoe.kt
@@ -0,0 +1,8 @@
+package bittar.rachel.theshoestore.models
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Shoe(var name: String, var size: Double, var company: String, var description: String,
+                val images: List<String> = mutableListOf()) : Parcelable
